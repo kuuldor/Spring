6b52e533b4a35bbff30f7ae0eb3d6bd973747dd5
Chengdong Lu <kuuldor@ymail.com>
2015-07-03 20:14:58 -0700

    Fix loading image nil for asyncButton

--- Spring/AsyncButton.swift
+++ Spring/AsyncButton.swift
@@ -35,13 +35,14 @@ public class AsyncButton: UIButton {
 
         if let urlString = url?.absoluteString {
             ImageLoader.sharedLoader.imageForUrl(urlString) { [weak self] image, url in
-
-                if let strongSelf = self {
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        if strongSelf.imageURL[state.rawValue]?.absoluteString == url {
-                            strongSelf.setImage(image, forState: state)
-                        }
-                    })
+                if image != nil {
+                    if let strongSelf = self {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            if strongSelf.imageURL[state.rawValue]?.absoluteString == url {
+                                strongSelf.setImage(image, forState: state)
+                            }
+                        })
+                    }
                 }
             }
         }
